ec4eacae5a3f58f8f627a5e5b047bd282590df3b
Tomasz Kudas <thomqq@gmail.com>
2020-05-19 07:45:56 +0200

    initial commit

---
+++ src/main/java/tq/arxsoft/dictant/DictantApplication.java
@@ -0,0 +1,13 @@
+package tq.arxsoft.dictant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tq/arxsoft/dictant/DictantApplicationTests.java
@@ -0,0 +1,13 @@
+package tq.arxsoft.dictant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
